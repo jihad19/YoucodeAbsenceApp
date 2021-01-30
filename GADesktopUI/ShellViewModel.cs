c3f4fa33d4f5b3b4367403c936a49ab7fd8e6aab
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-01-30 16:56:39 +0100

    Background glass blur effect and windowControl buttons

--- GADesktopUI/ShellViewModel.cs
+++ GADesktopUI/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace GADesktopUI
 {
-    public class ShellViewModel
+    public class ShellViewModel: Conductor<Screen>.Collection.OneActive
     {
+
+
+
+        //Windows Close button
+        public void closeWindow()
+        {
+
+            this.TryClose();
+        }
     }
 }
