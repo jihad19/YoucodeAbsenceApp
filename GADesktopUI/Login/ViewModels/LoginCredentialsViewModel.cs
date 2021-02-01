391028ace2d28cf7b7e36e4e7d2594f34649bd76
AYB-cmd <aeljelladi@gmail.com>
2021-02-01 11:49:33 +0100

    api for authentication

--- GADesktopUI/Login/ViewModels/LoginCredentialsViewModel.cs
+++ GADesktopUI/Login/ViewModels/LoginCredentialsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using GADesktopUI.APIHelpers;
 using GADesktopUI.Login.EventMessages;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,70 @@ namespace GADesktopUI.Login.ViewModels
     public class LoginCredentialsViewModel : Screen
     {
         private readonly IEventAggregator _eventAggregator;
-        public LoginCredentialsViewModel(IEventAggregator eventAggregator)
+        private IAPIHelper _apiHelper;
+        private string _userName;
+        private string _password;
+        //private string _errorMessage;
+        public LoginCredentialsViewModel(IAPIHelper apiHelper, IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
+            _apiHelper = apiHelper;
 
         }
 
-        public void LoginBtn()
+        public string PasswordInput
         {
-            _eventAggregator.PublishOnUIThread(new AttemptLogin());
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => PasswordInput);
+                NotifyOfPropertyChange(() => CanLoginBtn);
+            }
         }
+
+        public string UsernameInput
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UsernameInput);
+                NotifyOfPropertyChange(() => CanLoginBtn);
+            }
+        }
+
+
+        public bool CanLoginBtn
+        {
+            get
+            {
+                bool output = false;
+
+                if (UsernameInput?.Length > 0 && PasswordInput?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+
+        }
+        public async Task LoginBtn()
+        {
+
+            try
+            {
+                var result = await _apiHelper.Authenticate(UsernameInput, PasswordInput);
+                _eventAggregator.PublishOnUIThread(new AttemptLogin());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+       
     }
 }
