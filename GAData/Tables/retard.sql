1457f2e24e14a1eaca98d50168334e736509f6a9
Marouane Moumni <MAR@YC.com>
2021-02-01 16:38:12 +0200

    Added user-retard-absence tables

---
+++ GAData/Tables/retard.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[retard]
+(
+	[retard_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [dateheure] DATETIME NULL, 
+    [tempsretard] TIME NULL,
+    prof_ID int FOREIGN KEY REFERENCES professeur(prof_ID),
+    eleve_ID int FOREIGN KEY REFERENCES eleve(eleve_ID)
+)
