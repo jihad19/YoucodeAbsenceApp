63782c352609e16c4a79337e1b090857475e097a
AYB-cmd <aeljelladi@gmail.com>
2021-02-01 14:38:57 +0100

    loginModels events comunication

--- GADesktopUI/Login/ViewModels/LoginCredentialsViewModel.cs
+++ GADesktopUI/Login/ViewModels/LoginCredentialsViewModel.cs
@@ -12,14 +12,14 @@ namespace GADesktopUI.Login.ViewModels
     public class LoginCredentialsViewModel : Screen
     {
         private readonly IEventAggregator _eventAggregator;
-        private IAPIHelper _apiHelper;
+        //private IAPIHelper _apiHelper;
         private string _userName;
         private string _password;
-        //private string _errorMessage;
-        public LoginCredentialsViewModel(IAPIHelper apiHelper, IEventAggregator eventAggregator)
+        private string _errorMessage;
+        public LoginCredentialsViewModel( IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
-            _apiHelper = apiHelper;
+           
 
         }
 
@@ -45,6 +45,31 @@ namespace GADesktopUI.Login.ViewModels
             }
         }
 
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
 
         public bool CanLoginBtn
         {
@@ -61,18 +86,10 @@ namespace GADesktopUI.Login.ViewModels
             }
 
         }
-        public async Task LoginBtn()
+        public void LoginBtn()
         {
+            _eventAggregator.PublishOnUIThread(new AttemptLogin(UsernameInput, PasswordInput));
 
-            try
-            {
-                var result = await _apiHelper.Authenticate(UsernameInput, PasswordInput);
-                _eventAggregator.PublishOnUIThread(new AttemptLogin());
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
 
         }
 
