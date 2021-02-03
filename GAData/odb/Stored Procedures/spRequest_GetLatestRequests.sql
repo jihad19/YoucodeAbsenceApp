590a91b266837e0487fbdacca5f3b92977e72901
Marouane Moumni <MAR@YC.com>
2021-02-03 17:02:16 +0200

    Update SP

--- GAData/odb/Stored Procedures/spRequest_GetLatestRequests.sql
+++ GAData/odb/Stored Procedures/spRequest_GetLatestRequests.sql
@@ -1,8 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[spRequest_GetLatestRequests]
-	@FullName nvarchar(50),
+	@fullName nvarchar(50),
 	@abs_datetime datetime ,
 	@justificaion_name nvarchar(50)
 AS
 BEGIN
-	    SELECT t1.FullName, t1.email, t3.abs_datetime, t4.justification_name FROM dbo.[User] t1 inner join [student] t2 on t2.user_ID = t1.user_ID inner join [absence] t3 on t2.student_ID = t3.student_ID inner join [justification] t4 on t4.justification_ID = t3.justification_ID
+	SELECT t1.fullName , t3.abs_datetime, t4.justification_name 
+	FROM dbo.[User] t1 inner join [student] t2 on t2.[user_ID] = t1.[user_ID] inner join [absence] t3 on t2.student_ID = t3.student_ID inner join [justification] t4 on t4.justification_ID = t3.justification_ID 
+	WHERE t1.fullName = @fullName and t3.abs_datetime = @abs_datetime and t4.justification_name = @justificaion_name ;
 END
--- GAData/odb/Stored Procedures/spStudent_GetTodayAbsence.sql
+++ GAData/odb/Stored Procedures/spStudent_GetTodayAbsence.sql
@@ -1,10 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[spStudent_GetTodayAbsence]
-	@FullName nvarchar(50),
+	@fullName nvarchar(50),
 	@email nvarchar(50),
 	@abs_datetime datetime ,
 	@justificaion_name nvarchar(50)
 AS
 BEGIN
-	    SELECT t1.FullName, t1.email, t3.abs_datetime, t4.justification_name FROM dbo.[User] t1 inner join [student] t2 on t2.user_ID = t1.user_ID inner join [absence] t3 on t2.student_ID = t3.student_ID inner join [justification] t4 on t4.justification_ID = t3.justification_ID
+	    SELECT t1.FullName, t1.email, t3.abs_datetime, t4.justification_name 
+		FROM dbo.[User] t1 inner join [student] t2 on t2.[user_ID] = t1.[user_ID] inner join [absence] t3 on t2.student_ID = t3.student_ID inner join [justification] t4 on t4.justification_ID = t3.justification_ID
+		WHERE t1.fullName = @fullName and t1.email = @email and t3.abs_datetime = @abs_datetime and t4.justification_name = @justificaion_name ;
 END
 
