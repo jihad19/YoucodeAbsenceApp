f6a63073a58a9c5a8bfbf404871dae3e8225f7d3
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-02-09 10:45:13 +0100

    RecentRequests, and Teachers conponents

--- GADesktopUI/Content/ViewModels/AdminDashboardViewModel.cs
+++ GADesktopUI/Content/ViewModels/AdminDashboardViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using DemoDataAbsence;
+using DemoDataAbsence.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,16 @@ namespace GADesktopUI.Content.ViewModels
     public class AdminDashboardViewModel : Screen
     {
         private readonly IEventAggregator _eventAggregator;
-
+        public BindableCollection<StudentModel> Students { get; set; }
+        public BindableCollection<TeachersModel> Teachers { get; set; }
         public AdminDashboardViewModel(IEventAggregator eventAggregator)
         {
+            DemoDataAccess da = new DemoDataAccess();
+            Students = new BindableCollection<StudentModel>(da.GetStudents(3));
+            Teachers = new BindableCollection<TeachersModel>(da.GetTeachers());
             _eventAggregator = eventAggregator;
         }
+
+       
     }
 }
